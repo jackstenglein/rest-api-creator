a09fe80048dfaa39f73c41f63bd1b04ccb46be2d
Jack Stenglein <jackstenglein@utexas.edu>
2020-04-26 16:25:39 -0500

    frontend/endpoints: Add placeholder endpoints page

---
+++ frontend/src/endpoints/Endpoints.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Endpoints = () => (
+  <p>
+    This page currently does nothing. In a future version, you will be able to create custom endpoints, rather than using the default CRUD operations for all object types.
+  </p>
+)
+
+export default Endpoints;
