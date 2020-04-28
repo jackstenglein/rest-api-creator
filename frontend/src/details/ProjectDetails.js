631796c910b8ac88abe70ba153e59c3563936c8d
Jack Stenglein <jackstenglein@utexas.edu>
2020-04-28 17:16:05 -0500

    frontend/details: Connect ProjectDetails to Redux store

--- frontend/src/details/ProjectDetails.js
+++ frontend/src/details/ProjectDetails.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 
 // ProjectDetails returns the JSX for the project details page.
 const ProjectDetails = props => (
   <div>
-    <h2>Default Project</h2>
-    <p>This is the default project. Future versions of this website will allow you to create multiple projects. If you had created your own project,
-      a description of it would go here. For now, this site allows you to define database objects and auto-generate the code to support a REST API
-      based on those objects. Future versions will also allow you to automatically deploy the API to AWS and other cloud services. This page will 
-      allow you to see the status of those deployed APIs.
+    <h2>{props.project.name}</h2>
+    <p>{props.project.description}
     </p>
     <Button variant="primary" onClick={console.log("Download clicked")}>Download Code</Button>
   </div>
 )
 
-export default ProjectDetails;
+const mapStateToProps = state => {
+  return {
+    project: state.projects.defaultProject
+  }
+}
+
+export default connect(mapStateToProps)(ProjectDetails);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,11 @@ import { putObjectSuccess, putProjectSuccess } from './redux/modules/projects';
 const TEST_PROJECT = {
   id: "defaultProject",
   name: "Default Project",
-  description: "This is a sample project to test static UI. This project will eventually be dynamically pulled from Redux.",
+  description: "This is the default project. Future versions of this website will allow you to create multiple projects. " + 
+               "If you had created your own project, a description of it would go here. For now, this site allows you to " +
+               "define database objects and auto-generate the code to support a REST API based on those objects. Future " +
+               "versions will also allow you to automatically deploy the API to AWS and other cloud services. This page " +
+               "will allow you to see the status of those deployed APIs.",
   objects: {
     "user": {
       id: "user",
