d4d1746b3162daea5198f263389d2ee9bd56aaff
Jack Stenglein <jackstenglein@utexas.edu>
2020-04-29 22:06:32 -0500

    frontend/portal: Add interactivity to login screen

--- frontend/src/portal/Login.js
+++ frontend/src/portal/Login.js
@@ -15,9 +15,7 @@ const Input = props => (
           type={props.type}
           value={props.value}
           onChange={props.onChange}
-          isInvalid={props.error !== undefined && props.error.length > 0}
         />
-        <Form.Control.Feedback type="invalid">{props.error}</Form.Control.Feedback>
     </Col>
   </Form.Group>
 )
@@ -25,9 +23,9 @@ const Input = props => (
 const Login = props => (
   <Container className="mt-2">
     <h2>Login</h2>
-    <Input name="Email" placeholder="email@example.com" value={props.email} onChange={props.changeEmail} error={props.errors.email}/>
-    <Input name="Password" type="password" placeholder="Password" value={props.password} onChange={props.changePassword} error={props.errors.password}/>
-    <Button onClick={props.submit} disabled={!props.isValid}>Login</Button>
+    <Input name="Email" placeholder="email@example.com" value={props.email} onChange={props.changeEmail} />
+    <Input name="Password" type="password" placeholder="Password" value={props.password} onChange={props.changePassword} />
+    <Button onClick={props.login} disabled={!props.isValid}>Login</Button>
     <p className="mt-4">Don't have an account? <Link to="/">Signup</Link></p>
   </Container>
 )
--- frontend/src/portal/LoginContainer.js
+++ frontend/src/portal/LoginContainer.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import Login from './Login';
+import produce from 'immer';
 
 class LoginContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  changeEmail(event) {
+    const nextState = produce(this.state, draftState => {
+      draftState.email = event.target.value
+    });
+    this.setState(nextState);
+  }
+
+  changePassword(event) {
+    const nextState = produce(this.state, draftState => {
+      draftState.password = event.target.value
+    });
+    this.setState(nextState);
+  }
+
+  login() {
+    console.log(this.state);
+  }
+
   render() {
+    const isValid = this.state.email.length > 0 && this.state.password.length > 0;
     return (
       <Login 
-        email="test"
-        password="asdf"
-        errors={{}}
+        {...this.state}
+        isValid={isValid}
+        changeEmail={this.changeEmail}
+        changePassword={this.changePassword}
+        login={this.login}
       />
     )
   }
