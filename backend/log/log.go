fc1872a9305356e486129e4cf604bc22361e36fc
Jack Stenglein <jackstenglein@utexas.edu>
2020-05-19 11:51:55 -0500

    backend/log: Add improved logging functionality

---
+++ backend/log/log.go
@@ -0,0 +1,120 @@
+// Package log provides helpers for logging. It currently assumes that logs are written to AWS CloudWatch.
+// CloudWatch splits log entries using the newline character (\n), so the log package replaces newlines in
+// the middle of strings with carriage returns (\r). Log entries are ended with newlines.
+package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/jackstenglein/rest_api_creator/backend/errors"
+)
+
+// Log levels control which functions produce output and which are ignored.
+const (
+	// Silent allows logs from none of the logging functions.
+	Silent = iota
+
+	// Failure allows logs from the Fail function and the Error function when used with server errors.
+	Failure
+
+	// Warning allows logs from the Warn, Fail and Error functions.
+	Warning
+
+	// Debugging allows logs from the Debug, Warn, Fail and Error functions.
+	Debugging
+
+	// Information allows logs from all functions.
+	Information
+)
+
+// level contains the currently in-use log level.
+var level = Silent
+
+// writer contains the location to write logs.
+var writer io.Writer = os.Stdout
+
+// init uses the DEPLOYMENT_STAGE environment variable to set the log level. The log level can be overridden by using
+// the SetLevel function. If DEPLOYMENT_STAGE is not set or is invalid, the log level will be Silent.
+func init() {
+	switch os.Getenv("DEPLOYMENT_STAGE") {
+	case "dev":
+		level = Information
+	case "alpha":
+		level = Debugging
+	case "prod":
+		level = Failure
+	}
+}
+
+// printCarriageReturn formats its operands using their default formats. printCarriageReturn then replaces any
+// newlines in the resulting string with carriage returns. The final result is written to standard output and
+// a newline is appended.
+func printCarriageReturn(a ...interface{}) {
+	s := fmt.Sprintln(a...)
+	s = s[0 : len(s)-1]
+	s = strings.Replace(s, "\n", "\r", -1)
+	io.WriteString(writer, s)
+	io.WriteString(writer, "\n")
+}
+
+// SetLevel sets the log level to the provided value. If the given level is invalid, the current log
+// level is unchanged.
+func SetLevel(l int) {
+	if l >= Silent && l <= Information {
+		level = l
+	}
+}
+
+// Error writes the given error to standard output. If the error is nil, nothing is logged. If the error is a server error,
+// the log level must be Failure or higher for the error to be logged. If the error is a client error,
+// the log level must be Warning or higher for the error to be logged.
+func Error(err error) {
+	errString, status := errors.UserDetails(err)
+	if status == 200 {
+		return
+	}
+	if status == 500 {
+		Fail(errString)
+	} else if status == 400 {
+		Warn(errString)
+	}
+}
+
+// Fail formats using the default formats for its operands and writes to standard output if the log level is greater than or equal to
+// Failure. Spaces are added between operands.
+func Fail(a ...interface{}) {
+	if level >= Failure {
+		io.WriteString(writer, "[FAIL]: ")
+		printCarriageReturn(a...)
+	}
+}
+
+// Warn formats using the default formats for its operands and writes to standard output if the log level is greater than or equal to
+// Warning. Spaces are added between operands.
+func Warn(a ...interface{}) {
+	if level >= Warning {
+		io.WriteString(writer, "[WARN]: ")
+		printCarriageReturn(a...)
+	}
+}
+
+// Debug formats using the default formats for its operands and writes to standard output if the log level is greater than or equal to
+// Debugging. Spaces are added between operands.
+func Debug(a ...interface{}) {
+	if level >= Debugging {
+		io.WriteString(writer, "[DEBUG]: ")
+		printCarriageReturn(a...)
+	}
+}
+
+// Info formats using the default formats for its operands and writes to standard output if the log level is greater than or equal to
+// Information. Spaces are added between operands.
+func Info(a ...interface{}) {
+	if level >= Information {
+		io.WriteString(writer, "[INFO]: ")
+		printCarriageReturn(a...)
+	}
+}
---
+++ backend/log/log_test.go
@@ -0,0 +1,337 @@
+package log
+
+import (
+	"github.com/jackstenglein/rest_api_creator/backend/errors"
+	"strings"
+	"testing"
+)
+
+type errorTest struct {
+	name       string
+	err        error
+	wantString string
+}
+
+type logTest struct {
+	name       string
+	action     func(a ...interface{})
+	args       []interface{}
+	wantString string
+}
+
+var logTests = []struct {
+	name       string
+	level      int
+	errorTests []errorTest
+	logTests   []logTest
+}{
+	{
+		name:  "Silent",
+		level: Silent,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("ServerError"),
+				wantString: "",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("ClientError"),
+				wantString: "",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+		},
+	},
+	{
+		name:  "Failure",
+		level: Failure,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("ServerError"),
+				wantString: "[FAIL]: ServerError\n",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("ClientError"),
+				wantString: "",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[FAIL]: This is a test\n",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+		},
+	},
+	{
+		name:  "Warning",
+		level: Warning,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("Server\nError"),
+				wantString: "[FAIL]: Server\rError\n",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("Client\nError"),
+				wantString: "[WARN]: Client\rError\n",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[FAIL]: This is a test\n",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[WARN]: This is a test\n",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+		},
+	},
+	{
+		name:  "Debugging",
+		level: Debugging,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("Server\nError"),
+				wantString: "[FAIL]: Server\rError\n",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("Client\nError"),
+				wantString: "[WARN]: Client\rError\n",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[FAIL]: This is a test\n",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[WARN]: This is a test\n",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[DEBUG]: This is a test\n",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+		},
+	},
+	{
+		name:  "Information",
+		level: Information,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("Server\nError"),
+				wantString: "[FAIL]: Server\rError\n",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("Client\nError"),
+				wantString: "[WARN]: Client\rError\n",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[FAIL]: This is a test\n",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[WARN]: This is a test\n",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[DEBUG]: This is a test\n",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "[INFO]: This is a test\n",
+			},
+		},
+	},
+	{
+		name:  "Miscellaneous",
+		level: Information,
+		errorTests: []errorTest{
+			{
+				name:       "NilError",
+				err:        nil,
+				wantString: "",
+			},
+		},
+	},
+	{
+		name:  "InvalidLevel",
+		level: -1,
+		errorTests: []errorTest{
+			{
+				name:       "ServerError",
+				err:        errors.NewServer("ServerError"),
+				wantString: "",
+			},
+			{
+				name:       "ClientError",
+				err:        errors.NewClient("ClientError"),
+				wantString: "",
+			},
+		},
+		logTests: []logTest{
+			{
+				name:       "Fail",
+				action:     Fail,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Warn",
+				action:     Warn,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Debug",
+				action:     Debug,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+			{
+				name:       "Info",
+				action:     Info,
+				args:       []interface{}{"This", "is", "a", "test"},
+				wantString: "",
+			},
+		},
+	},
+}
+
+func TestLog(t *testing.T) {
+	for _, test := range logTests {
+		t.Run(test.name, func(t *testing.T) {
+			// Setup
+			SetLevel(test.level)
+			defer SetLevel(Silent)
+			var buf strings.Builder
+			writer = &buf
+
+			// Execute Error tests
+			for _, errorTest := range test.errorTests {
+				t.Run(errorTest.name, func(t *testing.T) {
+					// Setup
+					buf.Reset()
+
+					// Execute
+					Error(errorTest.err)
+
+					// Verify
+					gotString := buf.String()
+					if gotString != errorTest.wantString {
+						t.Errorf("Got string `%s`; want `%s`", gotString, errorTest.wantString)
+					}
+				})
+			}
+
+			// Execute log level tests
+			for _, logTest := range test.logTests {
+				t.Run(logTest.name, func(t *testing.T) {
+					// Setup
+					buf.Reset()
+
+					// Execute
+					logTest.action(logTest.args...)
+
+					// Verify
+					gotString := buf.String()
+					if gotString != logTest.wantString {
+						t.Errorf("Got string `%s`; want `%s`", gotString, logTest.wantString)
+					}
+				})
+			}
+		})
+	}
+}
